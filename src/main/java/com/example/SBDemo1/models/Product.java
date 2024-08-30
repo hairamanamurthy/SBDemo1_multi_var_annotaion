0c76d4cc7e9d318b9d9f914ea05f247362206650
Netti Ramana Murthy <hairamanamurthy@gmail.com>
2024-08-30 20:10:43 +0530

    Added Spring data jpa to project

--- src/main/java/com/example/SBDemo1/models/Product.java
+++ src/main/java/com/example/SBDemo1/models/Product.java
@@ -1,9 +1,13 @@
 package com.example.SBDemo1.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
 public class Product {
+    @Id
+    long id;
     String name;
     String category;
     String description;
